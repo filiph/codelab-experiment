48b636a01b31154e70ab9beaeee696e26cba075a
Filip Hracek <filip.hracek@gmail.com>
2022-12-02 09:05:30 +0100

    Fix test to at least compile

--- startups/test/widget_test.dart
+++ startups/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:startups/main.dart';
+import 'package:startups/main_9.dart' as final_step;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const final_step.MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
