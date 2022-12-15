ab390144d976b6719074a5e5d63886e18e657e8c
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 15:22:12 +0100

    Step 6 a business logic

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -32,6 +32,17 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
