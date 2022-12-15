553843a190e429337b4f1ce9eda9bac2f92710b6
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 14:11:57 +0100

    Step 05 a pair

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -38,12 +38,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase),
           ElevatedButton(
             onPressed: () {
               appState.getNext();
