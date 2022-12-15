00f6547947412cec3b959b7215038878691a2251
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 18:39:16 +0100

    step 7 d use selectedIndex

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -55,6 +55,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
     return Scaffold(
       body: Row(
         children: [
@@ -80,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page,
             ),
           ),
         ],
