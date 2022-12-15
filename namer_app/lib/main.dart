7f47b13e6b56156f6f724df0406eb612a55f3983
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 12:52:41 +0100

    Step 04 Your first behavior

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -27,6 +27,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -41,7 +46,7 @@ class MyHomePage extends StatelessWidget {
           Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext();
             },
             child: Text('Next'),
           ),
