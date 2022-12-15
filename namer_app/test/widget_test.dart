2b6db532f5f36412df25e6fe88e09de7be69191b
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 14:00:57 +0100

    Add test

--- namer_app/test/widget_test.dart
+++ namer_app/test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:namer_app/main.dart';
@@ -6,5 +7,29 @@ void main() {
   testWidgets('App starts', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
     expect(find.text('A random idea:'), findsOneWidget);
+  });
+
+  testWidgets('Tapping button changes word pair', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    String? findWordPair() {
+      final textWidgets = tester.widgetList<Text>(find.byType(Text));
+      // Skip the first widget ('A random idea') and take the next one.
+      final wordPairWidget = textWidgets.skip(1).first;
+      return wordPairWidget.data;
+    }
+
+    final firstPair = findWordPair();
+
+    // Sanity check. Finding the word pair twice will have the same result.
+    final firstPairDuplicate = findWordPair();
+    expect(firstPair, equals(firstPairDuplicate));
+
+    await tester.tap(find.text('Next'));
+    await tester.pumpAndSettle();
+
+    final nextPair = findWordPair();
+
+    expect(nextPair, isNot(firstPair));
   });
 }
