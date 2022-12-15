dcfdf092196aee0ce2fe35a71462b8de9b5114cd
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 12:48:53 +0100

    step04 add button

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -39,6 +39,12 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
