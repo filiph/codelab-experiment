46b5b59ca37f8df269d29cf2a0090185fba7b13d
Filip Hracek <filip.hracek@gmail.com>
2022-12-15 18:34:31 +0100

    step 7 c add selectedIndex

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -51,6 +51,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,9 +70,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 label: Text('Favorites'),
               ),
             ],
-            selectedIndex: 0,
+            selectedIndex: selectedIndex,
             onDestinationSelected: (value) {
-              print('selected: $value');
+              setState(() {
+                selectedIndex = value;
+              });
             },
           ),
           Expanded(
